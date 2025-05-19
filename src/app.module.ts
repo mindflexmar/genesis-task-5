ea2843ac17868d34df57c1f56290f22572529cc2
mindflexmar <156377558+mindflexmar@users.noreply.github.com>
2025-05-19 20:34:40 +0300

    add docker, docker-compose and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('DATABASE_HOST'),
+      port: +configService.get('DATABASE_PORT'),
+      username: configService.get('DATABASE_USERNAME'),
+      password: configService.get('DATABASE_PASSWORD'),
+      database: configService.get('DATABASE_NAME'),
+      entities: [],
+      synchronize: false,
+    }),
+    inject: [ConfigService]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
